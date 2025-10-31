6a910d12ed525ccf53a463860c1aad44ea1c7575
Kou2524 <yasumaruyamu@gmail.com>
2025-10-31 17:28:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def update():
 def draw():
     pyxel.cls(0)
     pyxel.text(30, 40, "CHAINSAW-MAN", 7)
-    pyxel.text(20, 70, "TAP SCREEN TO PLAY BGM", 10)
+    pyxel.text(20, 70, "", 10)
 
 pyxel.run(update, draw)
