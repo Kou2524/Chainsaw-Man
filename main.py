dd9a9f6fb8ef4dc3c6c28106cb3f22a670ef4548
Kou2524 <yasumaruyamu@gmail.com>
2025-11-02 15:40:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pyxel
 
-pyxel.init(160, 120, title="Chainsaw-Man")
 
 def update():
     pass
 
 def draw():
     pyxel.cls(0)
+    pyxel.init(160, 120, title="Chainsaw-Man")
     pyxel.text(50, 100, "PRESS START", 7)
 
 pyxel.run(update, draw)
