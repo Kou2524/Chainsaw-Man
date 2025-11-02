3d9644e6f9fa34a3bd148f581387371d061215f9
Kou2524 <yasumaruyamu@gmail.com>
2025-11-02 15:36:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import pyxel
 
 pyxel.init(160, 120, title="Chainsaw-Man")
 
-# ここで画像を読み込む（0番の画像バンクに入れる）
-pyxel.image(0).load(0, 0, "title.png")
-
 def update():
     pass
 
 def draw():
-    pyxel.cls(0)  # ここは基本あったほうがいい
-    # 読み込んだ画像を画面に貼る
-    pyxel.blt(0, 0, 0, 0, 0, 160, 120)
-    pyxel.text(20, 10, "CHAINSAW-MAN", 7)
+    pyxel.cls(0)
+    pyxel.text(50, 100, "PRESS START", 7)
 
 pyxel.run(update, draw)
